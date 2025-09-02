5928b293fd5cbb47bcb3932d7808b83aa2f624a2
Andrew7480 <andrewc7480@gmail.com>
2025-09-01 19:56:54 -0500

    base y minimo

--- Reto2/CarreraParalela.java
+++ Reto2/CarreraParalela.java
@@ -1,18 +1,28 @@
 //punto de partida
 
-//punto de partida
 package Reto2;
 import java.util.*;
 
 public class CarreraParalela {
     public static void main(String[] args) {
-        int mayor = numero();
-        System.out.println("El número es: " + mayor);
+        System.out.println("Ingrese los números separados por espacio:");
+        List<Integer> numeros = leerNumeros();
+    }
+    private static List<Integer> leerNumeros() {
+        Scanner scanner = new Scanner(System.in);
+        String linea = scanner.nextLine();
+        String[] partes = linea.split(" ");
+        List<Integer> numeros = new ArrayList<>();
+        for (String parte : partes) {
+            try {
+                numeros.add(Integer.parseInt(parte));
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida: " + parte);
+            }
+        }
+        return numeros;
     }
 
-    private static int numero(){
-        return 0;
-    }
     private static int minimoNumero(List<Integer> numeros){
         return numeros.stream()
                 .min(Integer::compareTo)
