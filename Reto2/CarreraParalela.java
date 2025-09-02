3ee9f1195edbb0867eee64c93682f89686193b53
Andrew7480 <andrewc7480@gmail.com>
2025-09-01 19:28:00 -0500

    inicial commit reto 2

--- Reto2/CarreraParalela.java
+++ Reto2/CarreraParalela.java
@@ -1,1 +1,18 @@
 //punto de partida
+
+//punto de partida
+package Reto2;
+import java.util.*;
+
+public class CarreraParalela {
+    public static void main(String[] args) {
+        int mayor = numero();
+        System.out.println("El número es: " + mayor);
+    }
+
+    private static int numero(){
+        return 0;
+    }
+}
+
+
