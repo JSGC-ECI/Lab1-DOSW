bb05222544830f37063aa6e525d59b7894dd7c99
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2025-09-04 16:53:40 -0500

    Version

--- Reto3/Eco.java
+++ Reto3/Eco.java
@@ -1,5 +1,17 @@
 package Reto3;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 public class Eco {
-    
+    public String invertirMensaje(String mensaje) {
+        return new StringBuffer(mensaje).reverse().toString();
+    }
+
+    public String ecoMisterioso(String mensaje) {
+        return invertirMensaje(IntStream.range(0, 3)
+                .mapToObj(i -> mensaje)
+                .collect(Collectors.joining(" ")));
+    }
+
 }
