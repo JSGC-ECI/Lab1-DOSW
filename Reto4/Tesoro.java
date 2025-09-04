83af2897d5edb3d18daf89eb0035c20674bdf367
Andrew7480 <andrewc7480@gmail.com>
2025-09-04 18:33:02 -0500

    reto 4: estudiante b

--- Reto4/Tesoro.java
+++ Reto4/Tesoro.java
@@ -2,13 +2,22 @@ package Reto4;
 import java.util.stream.*;
 import java.util.*;
 public class Tesoro {
-    private static Hashtable<String, Integer> tesoro = new Hashtable<>();
+    private static Hashtable<String, Integer> tesorosHashtable = new Hashtable<>();
 
-    public static void almacenar(List<Map.Entry<String, Integer>> pares){
+    public static void almacenarHashtable(List<Map.Entry<String, Integer>> pares){
         
         for(Map.Entry<String, Integer> par : pares){
-            tesoro.putIfAbsent(par.getKey(), par.getValue());
+            tesorosHashtable.putIfAbsent(par.getKey(), par.getValue());
         }
 
     }
+    public static Map<String, Integer> obtenerTesoro(HashMap<String, Integer> tesorosHashMap,Hashtable<String, Integer> tesorosHashtable){
+            Map<String, Integer> tesoroComun = new HashMap<>(tesorosHashMap);
+
+            tesorosHashtable.entrySet().stream()
+            .forEach(e -> tesoroComun.put(e.getKey(), e.getValue()));
+
+            return tesoroComun;
+    }
+
 }
