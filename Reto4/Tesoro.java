448d84d7c248a8ba1cfa5445839933f68cd76edd
Andrew7480 <andrewc7480@gmail.com>
2025-09-04 17:49:18 -0500

    parte 1

--- Reto4/Tesoro.java
+++ Reto4/Tesoro.java
@@ -2,5 +2,13 @@ package Reto4;
 import java.util.stream.*;
 import java.util.*;
 public class Tesoro {
-    
+    private static Hashtable<String, Integer> tesoro = new Hashtable<>();
+
+    public static void almacenar(List<Map.Entry<String, Integer>> pares){
+        
+        for(Map.Entry<String, Integer> par : pares){
+            tesoro.putIfAbsent(par.getKey(), par.getValue());
+        }
+
+    }
 }
