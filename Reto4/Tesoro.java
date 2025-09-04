b9e22a03516439078c392891b94afec86e731208
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2025-09-04 18:24:34 -0500

    Firma

--- Reto4/Tesoro.java
+++ Reto4/Tesoro.java
@@ -3,18 +3,12 @@ import java.util.*;
 
 public class Tesoro {
 
-    private static Map<String, Integer> almacenar = new HashMap<>();
+    private static Map<String, Integer> tesoro = new HashMap<>();
 
     public static void almacenar(List<Map.Entry<String, Integer>> pares) {
         for (Map.Entry<String, Integer> par : pares) {
             almacenar.putIfAbsent(par.getKey(), par.getValue());
         }
-    }
-
-    public void mostrarHashMap() {
-        hashMap.forEach((k, v) ->
-                System.out.println("Clave: " + k + " | Valor: " + v)
-        );
     }
 
     public static Map<String, Integer> obtenerTesoro(HashMap<String, Integer> tesorosHashMap,Hashtable<String, Integer> tesorosHashtable)
