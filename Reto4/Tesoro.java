12b5ad87633f627ec374175e9b935b184200e894
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2025-09-04 17:35:58 -0500

    Lista almacenar

--- Reto4/Tesoro.java
+++ Reto4/Tesoro.java
@@ -2,5 +2,10 @@ package Reto4;
 import java.util.stream.*;
 import java.util.*;
 public class Tesoro {
-    
+    public static Map<String, Integer> almacenar(List<Map.Entry<String, Integer>> pares) {
+        Map<String, Integer> mapa = new HashMap<>();
+        for (Map.Entry<String, Integer> par : pares) {
+            mapa.putIfAbsent(par.getKey(), par.getValue()); // conserva el primero
+        }
+        return mapa;
 }
