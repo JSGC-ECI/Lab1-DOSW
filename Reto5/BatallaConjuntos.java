8e8d1286f20cf31b5d0156058152a11429312ca3
Andrew7480 <andrewc7480@gmail.com>
2025-09-04 19:49:13 -0500

    estudiante B

--- Reto5/BatallaConjuntos.java
+++ Reto5/BatallaConjuntos.java
@@ -1,5 +1,14 @@
 package Reto5;
 
+import java.util.List;
+
+import java.util.*;
 public class BatallaConjuntos {
-    
+    private static TreeSet<Integer> resultado = new TreeSet<>();
+
+
+    public static TreeSet<Integer> almacenar(List<Integer> numeros) {
+		numeros.stream().filter(i -> i % 5 != 0).forEach(resultado::add);
+		return resultado;
+	}
 }
