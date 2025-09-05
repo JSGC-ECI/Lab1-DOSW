63210447b644f173a48519853a5ff426c41a04b2
Andrew7480 <andrewc7480@gmail.com>
2025-09-04 19:56:50 -0500

    estudiante b

--- Reto5/BatallaConjuntos.java
+++ Reto5/BatallaConjuntos.java
@@ -11,4 +11,9 @@ public class BatallaConjuntos {
 		numeros.stream().filter(i -> i % 5 != 0).forEach(resultado::add);
 		return resultado;
 	}
+    public static TreeSet<Integer> unir( HashSet<Integer> conjunto1, TreeSet<Integer> conjunto2) {
+        TreeSet<Integer> result= new TreeSet<>(conjunto2);
+        result.stream().forEach(conjunto1::add);
+        return result;
+    }
 }
