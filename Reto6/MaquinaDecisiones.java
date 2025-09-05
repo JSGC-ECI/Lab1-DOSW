401488762aa4d0745ef19345e117a27bf4a5c0e5
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2025-09-04 21:29:20 -0500

    Parte

--- Reto6/MaquinaDecisiones.java
+++ Reto6/MaquinaDecisiones.java
@@ -2,11 +2,25 @@ package Reto6;
 
 public class MaquinaDecisiones {
 
-
-
-
-    public static void main(String[] args) {
-        
+        public static void decisiones(String decision) {
+            switch (comando) {
+                case "SALUDAR":
+                    System.out.println("La máquina dice: ¡Saludos, viajero del tiempo y del código!");
+                    break;
+                case "DESPEDIR":
+                    System.out.println("La máquina dice: Que los bits te acompañen, hasta la próxima misión.");
+                    break;
+                case "CANTAR":
+                    System.out.println("La máquina canta: 01010101");
+                    break;
+                case "DANZAR":
+                    System.out.println("La máquina gira y emite chispas: Girando en modo fiesta.");
+                    break;
+                default:
+                    System.out.println("Comando no reconocido: " + comando);
+            }
+        }
     }
+
     
 }
