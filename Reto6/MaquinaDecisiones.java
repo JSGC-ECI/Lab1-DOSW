8edd3c5be0a778085806b3f2ca242ef6d0cbd8b4
Andrew7480 <andrewc7480@gmail.com>
2025-09-04 21:23:06 -0500

    estudiante a

--- Reto6/MaquinaDecisiones.java
+++ Reto6/MaquinaDecisiones.java
@@ -3,10 +3,29 @@ package Reto6;
 public class MaquinaDecisiones {
 
 
+    public static void decisiones(String decision) {
+        switch (decision.toUpperCase()) {
+            case "SALUDAR":
+                System.out.println("¡Saludos, viajero del tiempo y del código!");
+                break;
+            case "DESPEDIR":
+                System.out.println("Que los bits te acompañen, hasta la próxima misión.");
+                break;
+            case "CANTAR":
+                System.out.println("01010101");
+                break;
+            case "DANZAR":
+                System.out.println("Girando en modo fiesta.");
+                break;
 
+            default:
+                System.out.println("Decisión no reconocida.");
+                break;
+        }
+    }
 
     public static void main(String[] args) {
-        
+        decisiones("SALUDAR");
     }
     
 }
